18ca9261bcea942116891bb97370456146417e36
velip <anders_teehee@hotmail.no>
2025-05-15 09:43:13 +0200

    Update config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 // Server configuration
-export const API_URL = 'http://192.168.1.163:5000';
+export const API_URL = 'http://localhost:5000';
 
 // You can change the IP address here to point to your server
 // Example: export const API_URL = 'http://192.168.1.100:5000'; 
